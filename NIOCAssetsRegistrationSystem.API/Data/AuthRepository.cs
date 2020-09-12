d3b80c3064d3f8c390831bf37bd5e1c1f39aac8b
Ali <aliii1984@outlook.com>
2020-09-12 09:44:37 +0430

    Auth Reg Updated

--- NIOCAssetsRegistrationSystem.API/Data/AuthRepository.cs
+++ NIOCAssetsRegistrationSystem.API/Data/AuthRepository.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NIOCAssetsRegistrationSystem.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NIOCAssetsRegistrationSystem.API.Data
 {
-    public class AuthRepository: IAuthRepository
+    public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.UserName == username))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
     }
 }
