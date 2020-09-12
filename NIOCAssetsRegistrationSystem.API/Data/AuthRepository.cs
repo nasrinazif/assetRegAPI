da445f236a441d3747d66bb11455d2224d3c28b7
Ali <aliii1984@outlook.com>
2020-09-12 09:58:14 +0430

    Authrepo Register added

--- NIOCAssetsRegistrationSystem.API/Data/AuthRepository.cs
+++ NIOCAssetsRegistrationSystem.API/Data/AuthRepository.cs
@@ -20,9 +20,19 @@ namespace NIOCAssetsRegistrationSystem.API.Data
             throw new NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
         }
 
         public  async Task<bool> UserExists(string username)
@@ -41,6 +51,22 @@ namespace NIOCAssetsRegistrationSystem.API.Data
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
     }
