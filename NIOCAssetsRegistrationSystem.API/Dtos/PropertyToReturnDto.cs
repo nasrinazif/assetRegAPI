71905ff23e238fb1c95cdc13d2a21b1415d27285
Ali <aliii1984@outlook.com>
2020-09-15 09:08:12 +0430

    PropertyToReturnDto Added

---
+++ NIOCAssetsRegistrationSystem.API/Dtos/PropertyToReturnDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIOCAssetsRegistrationSystem.API.Dtos
+{
+    public class PropertyToReturnDto
+    {
+        public int Id { get; set; }
+        public string PropertyTitle { get; set; }
+        public decimal? ArenaArea { get; set; }
+        public bool? OwnershipDocument { get; set; }
+        public bool? ExistingMap { get; set; }
+        public bool? ExistingBuilding { get; set; }
+        public decimal? BuildingArea { get; set; }
+        public string Description { get; set; }
+        public DateTime? LatestChanges { get; set; }
+        public byte[] UploadedFile { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
