64fab07bcb20b5241de37c1b64edef6658891ef7
Ali <aliii1984@outlook.com>
2020-09-28 08:41:56 +0330

    File Upload Model Added

---
+++ NIOCAssetsRegistrationSystem.API/Models/FileUpload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIOCAssetsRegistrationSystem.API.Models
+{
+    public class FileUpload
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public Company Company { get; set; }
+        public int? CompanyId { get; set; }
+        public User User { get; set; }
+        public int? UserId { get; set; }
+        public DateTime? FileUploadDate { get; set; }
+        public string Description { get; set; }
+        public byte[] UploadedFile { get; set; }
+    }
+}
